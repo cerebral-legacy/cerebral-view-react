6162d5d1a806ed947f49867528f64f49f76a2b51
christianalfoni <christianalfoni@gmail.com>
2015-08-16 22:27:50 +0200

    Fixed reference to controller

--- src/mixin.js
+++ src/mixin.js
@@ -1,22 +1,29 @@
 var React = require('react');
 
+var getValue = function (path, obj) {
+  path = path.slice();
+  while (path.length) {
+    obj = obj[path.shift()];
+  }
+  return obj;
+};
+
 module.exports = {
   contextTypes: {
     controller: React.PropTypes.object
   },
   componentWillMount: function () {
-    console.log(this.context);
     this.signals = this.context.controller.signals;
     this.recorder = this.context.controller.recorder;
     this.get = this.context.controller.get;
-    this.context.controller.eventEmitter.on('change', this._update);
-    this.context.controller.eventEmitter.on('remember', this._update);
+    this.context.controller.on('change', this._update);
+    this.context.controller.on('remember', this._update);
     this._update(this.context.controller.get([]));
   },
   componentWillUnmount: function () {
     this._isUmounting = true;
-    this.context.controller.eventEmitter.removeListener('change', this._update);
-    this.context.controller.eventEmitter.removeListener('remember', this._update);
+    this.context.controller.removeListener('change', this._update);
+    this.context.controller.removeListener('remember', this._update);
   },
   shouldComponentUpdate: function (nextProps, nextState) {
     var propKeys = Object.keys(nextProps);
@@ -40,13 +47,14 @@ module.exports = {
 
     return false;
   },
-  _update: function (state) {
+  _update: function () {
     if (this._isUmounting || !this.getStatePaths) {
       return;
     }
     var statePaths = this.getStatePaths();
+    var state = this.context.controller.get();
     var newState = Object.keys(statePaths).reduce(function (newState, key) {
-      newState[key] = Value(statePaths[key], state);
+      newState[key] = getValue(statePaths[key], state);
       return newState;
     }, {});
     this.setState(newState);
