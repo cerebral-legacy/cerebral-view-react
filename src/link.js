f9f5c2b6aa6b3006d985a059c9ef04dcdd704934
Alexey Gurianov <gurianov@gmail.com>
2016-02-24 16:35:12 +0400

    fix(Link): check signal prop on each render
    fixes #48

--- src/link.js
+++ src/link.js
@@ -16,24 +16,6 @@ module.exports = React.createClass({
   },
 
   componentWillMount: function () {
-    var controller = this.context.controller
-
-    if (typeof this.props.signal === 'function') {
-      this.signal = this.props.signal
-      this.signalName = this.signal.signalName
-    } else {
-      this.signalName = this.props.signal
-      this.signal = get(controller.getSignals(), this.signalName)
-    }
-
-    var routerMeta = controller.getModules()['cerebral-module-router']
-    if (routerMeta) {
-      this.router = get(controller.getServices(), routerMeta.name)
-    }
-
-    if (typeof this.signal !== 'function') {
-      throw new Error('Cerebral React - You have to pass a signal or signal name to the Link component')
-    }
   },
 
   onClick: function (e) {
@@ -42,16 +24,38 @@ module.exports = React.createClass({
   },
 
   render: function () {
+    var controller = this.context.controller
+    var router
+    var signal
+    var signalName
+
+    if (typeof this.props.signal === 'function') {
+      signal = this.signal = this.props.signal
+      signalName = signal.signalName
+    } else {
+      signalName = this.props.signal
+      signal = this.signal = get(controller.getSignals(), signalName)
+    }
+
+    var routerMeta = controller.getModules()['cerebral-module-router']
+    if (routerMeta) {
+      router = get(controller.getServices(), routerMeta.name)
+    }
+
+    if (typeof signal !== 'function') {
+      throw new Error('Cerebral React - You have to pass a signal or signal name to the Link component')
+    }
+
     var passedProps = this.props
     var props = Object.keys(passedProps).reduce(function (props, key) {
       props[key] = passedProps[key]
       return props
     }, {})
 
-    if (this.router && typeof this.router.getSignalUrl === 'function') {
-      props.href = this.router.getSignalUrl(this.signalName, this.props.params) || undefined
-    } else if (typeof this.signal.getUrl === 'function') {
-      props.href = this.signal.getUrl(this.props.params || {})
+    if (router && typeof router.getSignalUrl === 'function') {
+      props.href = router.getSignalUrl(signalName, this.props.params) || undefined
+    } else if (typeof signal.getUrl === 'function') {
+      props.href = signal.getUrl(this.props.params || {})
     }
     if (!props.href) {
       props.onClick = this.onClick
